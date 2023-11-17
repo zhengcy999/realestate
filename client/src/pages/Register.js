dc0134b61695412a8a03500a4f0290d24f6901dd
zheng <zhengchuyi93@gmail.com>
2023-11-17 14:05:29 -0800

    Create the reigsiter form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,39 @@
 export default function Register(){
+
+    const handleSubmit=async()=>{
+        try{
+            //
+        }
+        catch(err){
+            console.log(err);
+        }
+
+    }
     return (
         <div>
             <h1 className="display-1 bg-primary text-lgiht p-5">
             Register
             </h1>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4 offset-lg-4">
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" placeholder="Enter your email"
+                            className="form-control"
+                            required
+                            autoFocus />
+                            <input type="password" placeholder="Enter your password"
+                            className="form-control mb-4"
+                            required
+                            autoFocus />
+                          <button className="btn btn-primary col-12 mb-4">Register</button>
+                          
+                        </form>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
